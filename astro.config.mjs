7d7bbce449211dd2b1362fa6d3c968d1d8f30c55
V K <84337571+1e9y@users.noreply.github.com>
2024-01-26 07:21:23 +0100

    ad astra

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('astro/config').AstroUserConfig} */
+export default {
+  // Set the site directory to `site`.
+  // The rest of the directory names are whole words, and I don't want `src` to stand out.
+  srcDir: "site",
+
+  // Do not minify HTML. Let the source be readable!
+  compressHTML: false,
+};
